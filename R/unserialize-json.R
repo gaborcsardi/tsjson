728d104002fd07528b32b9e90811a6efcc730c86
Gábor Csárdi <csardi.gabor@gmail.com>
2025-08-27 13:52:45 +0200

    parse_json -> unserialize_json

--- R/unserialize-json.R
+++ R/unserialize-json.R
@@ -1,6 +1,6 @@
 #' @export
 
-parse_json <- function(file = NULL, text = NULL) {
+unserialize_json <- function(file = NULL, text = NULL) {
   # parse file/text
   # TODO: error on error, get error position
   tt <- token_table(file = file, text = text)
@@ -17,52 +17,52 @@ parse_json <- function(file = NULL, text = NULL) {
   itop <- itop[tt$type[itop] != "comment"]
   stopifnot(length(itop) == 1)
 
-  parse_element(tt, itop)
+  unserialize_element(tt, itop)
 }
 
-parse_element <- function(token_table, id) {
+unserialize_element <- function(token_table, id) {
   switch(
     token_table$type[id],
     null = {
-      parse_null(token_table, id)
+      unserialize_null(token_table, id)
     },
     true = {
-      parse_true(token_table, id)
+      unserialize_true(token_table, id)
     },
     false = {
-      parse_false(token_table, id)
+      unserialize_false(token_table, id)
     },
     string = {
-      parse_string(token_table, id)
+      unserialize_string(token_table, id)
     },
     number = {
-      parse_number(token_table, id)
+      unserialize_number(token_table, id)
     },
     array = {
-      parse_array(token_table, id)
+      unserialize_array(token_table, id)
     },
     object = {
-      parse_object(token_table, id)
+      unserialize_object(token_table, id)
     }
   )
 }
 
-parse_null <- function(token_table, id) {
+unserialize_null <- function(token_table, id) {
   stopifnot(token_table$type[id] == "null")
   NULL
 }
 
-parse_true <- function(token_table, id) {
+unserialize_true <- function(token_table, id) {
   stopifnot(token_table$type[id] == "true")
   TRUE
 }
 
-parse_false <- function(token_table, id) {
+unserialize_false <- function(token_table, id) {
   stopifnot(token_table$type[id] == "false")
   FALSE
 }
 
-parse_string <- function(token_table, id) {
+unserialize_string <- function(token_table, id) {
   stopifnot(token_table$type[id] == "string")
   # escapes are almost the same as for R, but R does not have \/
   chdn <- token_table$children[[id]]
@@ -72,25 +72,25 @@ parse_string <- function(token_table, id) {
   eval(parse(text = str, keep.source = FALSE))
 }
 
-parse_number <- function(token_table, id) {
+unserialize_number <- function(token_table, id) {
   stopifnot(token_table$type[id] == "number")
   # single token
   as.numeric(token_table$code[id])
 }
 
-parse_array <- function(token_table, id) {
+unserialize_array <- function(token_table, id) {
   stopifnot(token_table$type[id] == "array")
   chdn <- token_table$children[[id]]
   # drop [ and , and ], parse the rest
   chdn <- chdn[!token_table$type[chdn] %in% c("[", ",", "]", "comment")]
   arr <- vector("list", length(chdn))
   for (idx in seq_along(chdn)) {
-    arr[[idx]] <- parse_element(token_table, chdn[idx])
+    arr[[idx]] <- unserialize_element(token_table, chdn[idx])
   }
   arr
 }
 
-parse_object <- function(token_table, id) {
+unserialize_object <- function(token_table, id) {
   stopifnot(token_table$type[id] == "object")
   # keep the pairs, parse their names and values
   chdn <- token_table$children[[id]]
@@ -101,9 +101,9 @@ parse_object <- function(token_table, id) {
     gchdn <- token_table$children[[chdn[idx]]]
     gchdn <- gchdn[!is.na(token_table$field_name[gchdn])]
     key <- gchdn[token_table$field_name[gchdn] == "key"]
-    nms[idx] <- parse_string(token_table, key)
+    nms[idx] <- unserialize_string(token_table, key)
     value <- gchdn[token_table$field_name[gchdn] == "value"]
-    arr[[idx]] <- parse_element(token_table, value)
+    arr[[idx]] <- unserialize_element(token_table, value)
   }
   structure(arr, names = nms)
 }
