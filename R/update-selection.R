42b90b38fab2d1bd9b7db0e0d4a79a16ba76ba76
Gábor Csárdi <csardi.gabor@gmail.com>
2025-08-29 00:22:46 +0200

    Update

---
+++ R/update-selection.R
@@ -0,0 +1,42 @@
+#' @export
+
+update_selection <- function(json, new) {
+  select <- attr(json, "selection")
+  if (is.null(select)) {
+    stop(cnd(
+      "No selection to delete from JSON object. Use `select()` first \\
+       to select the elements to delete."
+    ))
+  }
+
+  if (length(select) == 0) {
+    return(json)
+  }
+
+  deleted <- select
+  while (TRUE) {
+    deleted2 <- unique(c(deleted, unlist(json$children[deleted])))
+    if (length(deleted2) == length(deleted)) {
+      break
+    }
+    deleted <- deleted2
+  }
+
+  # keep select nodes to inject the new elements
+  code <- serialize_json(new, collapse = TRUE)
+  json$code[select] <- NA_character_
+  json$tws[select] <- code
+  really_deleted <- setdiff(deleted, select)
+  if (length(really_deleted)) {
+    json <- json[-really_deleted, ]
+  }
+
+  parts <- c(rbind(json$code, json$tws))
+  text <- unlist(lapply(na_omit(parts), charToRaw))
+
+  # TODO: update coordinates without reparsing
+  new <- load_json(text = text)
+  attr(new, "file") <- attr(json, "file")
+
+  new
+}
