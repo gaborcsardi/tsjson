6b7edb7c11812341725392e83b10620ea65e9e8f
Gábor Csárdi <csardi.gabor@gmail.com>
2025-09-03 14:17:44 +0200

    Import utils::head, utils::tail

--- R/tsjson-package.R
+++ R/tsjson-package.R
@@ -1,6 +1,9 @@
 #' @keywords internal
 "_PACKAGE"
 
+#' @importFrom utils head tail
+NULL
+
 ## usethis namespace: start
 #' @useDynLib tsjson, .registration = TRUE, .fixes = "c_"
 ## usethis namespace: end
